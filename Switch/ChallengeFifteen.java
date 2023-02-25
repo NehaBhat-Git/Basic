25cc4f1b6d39c79bed1d111278bef2b41f7a3f58
NehaBhat-Git <89778970+NehaBhat-Git@users.noreply.github.com>
2023-02-25 21:17:18 +0530

    Add files via upload

---
+++ Switch/ChallengeFifteen.java
@@ -0,0 +1,24 @@
+public class ChallengeFifteen {
+    public static void main(String []args){
+        System.out.println(sumOdd(100,1000));
+    }
+    public static boolean isOdd (int number){
+       if (number < 0){
+           return false;
+       }
+        return number % 2 != 0;
+    }
+    public static int sumOdd(int start, int end){
+        int sumOdd = 0;
+        if((start<0)||(end<0)) {
+            return -1;
+        }
+        for(int i =start; i <=end; i++){
+            if(isOdd(i)){
+                sumOdd+=i;
+            }
+        }
+        return sumOdd;
+    }
+}
+
