13c042f2cd17f390b1a953dc9773d647638f570f
NehaBhat-Git <89778970+NehaBhat-Git@users.noreply.github.com>
2023-02-27 20:29:22 +0530

    Add files via upload

---
+++ Switch/ChallengeNineteen.java
@@ -0,0 +1,12 @@
+public class ChallengeNineteen {
+    public static void main(String[] args) {
+
+    }
+    public static boolean hasSharedDigit(int numOne, int numTwo){
+        if(numOne > 10 && numTwo < 99)
+        {
+           return false;
+        }
+        return true;
+    }
+}
